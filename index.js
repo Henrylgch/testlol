3c94ec0dca9f9bca24799b0e31bd6cdbbd76d6e7
Henry L Garcia <henrylgch@gmail.com>
2019-08-08 17:23:14 -0400

    Server deberia funcionar en produccion

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http')
-const fs = require('fs')
+const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  fs.readFile("./public/index.html", (err, data) => {
+  fs.readFile('./public/index.html', (err, data) => {
     if (err) {console.error(err); return;}
     res.end(data)
-  }) 
+  })
 })
 
-var port = process.env.PORT || 1337;
-server.listen(3000)
+server.listen(3000);
